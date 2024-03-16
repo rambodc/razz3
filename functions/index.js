bcc026164c04af2fd2a5a8653c1f812e776c3f42
rambodc <rambodc@irobotx.io>
2024-03-16 04:22:10 +0000

    Small update in functions

--- functions/index.js
+++ functions/index.js
@@ -4,7 +4,7 @@ const cors = require("cors")({origin: true});
 exports.getExample = functions.https.onRequest((request, response) => {
     cors(request, response, () => {
         if (request.method === 'GET') {
-            response.send('Hello, this is a GET request!');
+            response.send('Hello, this is a GET request 2!');
         } else {
             response.status(405).send('Method Not Allowed');
         }
